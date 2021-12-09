440c6d7b1e6846a0c145f54f9ca16aa96bdd7020
Valerian Rykov <Valerra2@gmail.com>
2021-12-09 11:05:01 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 import style from "./App.module.css";
 import Section from "./components/Section";
 import Statistics from "./components/Statistics";
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ProtType from "prop-types";
 import style from "./Statistics.module.css";
 
